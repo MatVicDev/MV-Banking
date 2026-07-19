15e2340b7e38edc0ec6a27120791e671d8a874f7
Matheus Victor <matheusvic013@gmail.com>
2026-07-19 13:47:21 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/mvbanking/MvbankingApplication.java
@@ -0,0 +1,13 @@
+package br.com.mvbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvbankingApplication.class, args);
+	}
+
+}
